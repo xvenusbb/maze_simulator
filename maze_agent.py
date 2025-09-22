4103e80c6230492a469a83a3cb15bfa7997e3a31
Clara Mendes <marrferra01@gmail.com>
2025-09-22 20:34:49 -0300

    new commit

--- maze_agent.py
+++ maze_agent.py
@@ -29,5 +29,45 @@ class Environment:
                 if cell == 'E':
                     return (i,j)
             return None
+    
+    def count_food(self):
+        if not self.maze:
+            return 0
+        count = 0
+        for row in self.maze:
+            count += row.count ('o')
+            return count
         
+    def get_sensor(self, pos, direction):
+        sensor_matrix = [['X' for _ in range (3)] for _ in range (3)]
+
+        dir_offsets = {
+            'N': [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),  (0, 0),  (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)],
+            'S': [(1, 1),   (1, 0),   (1, -1),
+                  (0, 1),   (0, 0),   (0, -1),
+                  (-1, 1),  (-1, 0),  (-1, -1)],
+            'L': [(-1, 1),  (0, 1),   (1, 1),
+                  (-1, 0),  (0, 0),   (1, 0),
+                  (-1, -1), (0, -1),  (1, -1)],
+            'O': [(1, -1),  (0, -1),  (-1, -1),
+                  (1, 0),   (0, 0),   (-1, 0),
+                  (1, 1),   (0, 1),   (-1, 1)]
+        }
+    offsets = dir_offsets[direction]
+
+    for i in range(3):
+            for j in range(3):
+                dy, dx = offsets[i * 3 + j]
+                new_y, new_x = pos[0] + dy, pos[1] + dx
+                
+                if i == 1 and j == 1:  
+                    sensor_matrix[i][j] = direction
+                elif (0 <= new_y < len(self.maze) and 
+                      0 <= new_x < len(self.maze[0])):
+                    sensor_matrix[i][j] = self.maze[new_y][new_x]
+                else:
+                    sensor_matrix[i][j] = 'X' 
         
+    return sensor_matrix # type: ignore
