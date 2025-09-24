df56a74166c1dc9953bb7c3b19315b21325dc0b6
Clara Mendes <marrferra01@gmail.com>
2025-09-23 22:39:00 -0300

    New elements from amze_agent

--- maze_agent.py
+++ maze_agent.py
@@ -70,4 +70,98 @@ class Environment:
                 else:
                     sensor_matrix[i][j] = 'X' 
         
-    return sensor_matrix # type: ignore
+                return sensor_matrix # type: ignore
+
+    def set_direction(self,direction):
+        self.agent_direction = direction
+    
+    def move (self):
+        return self.move_agent(self.agent_direction)
+    
+    def move_agent(self, direction):
+        self.agent_direction = direction
+        moves = {'N': (-1, 0), 'S': (1, 0), 'L': (0, 1), 'O': (0, -1)}
+        dy, dx = moves[direction]
+        
+        new_x =self.agent_pos[0] + dx
+        new_y = self.agent_pos[0] +dy
+
+        if (0<= new_x < len(self.maze and 0 <= new_y < len(self.maze[0] and self.maze[new_y][new_x])) != 'X'):
+
+            self.agent_pos = (new_y, new_x)
+            self.steps += 1
+            return True
+        return False
+    
+    def get_current_cel(self):
+        return self.maze[self.agent_pos[0]][self.agent_pos[1]] == 'S'
+    
+    def print_maze(self):
+        direction_symbols = {'N': '↑', 'S': '↓', 'L': '→', 'O': '←'}
+
+        for i, row in enumerate(self.maze):
+            for j, cell in enumerate(row):
+                if (i, j) == self.agent_pos:
+                    print(direction_symbols.get (self.agent_direction, 'A'), end = '')
+                
+                else:
+                    print(cell, end='')
+                print()
+            print()
+
+class Agent:
+    def __init__(self, total_food):
+        self.total_food = total_food
+        self.collected_food = 0
+        self.direction = 'N'
+        self.memory = {}
+        self.visited = set()
+        self.path_history = []
+        self.food_locations = set()
+        self.exit_location = None
+
+    def get_sensor(self):
+        pass
+
+    def set_direction(self,direction):
+        self.direction = direction
+
+    def move (self):
+        pass
+
+    def update_memory(self, sensor_data, current_pos):
+          directions = ['N', 'S', 'L', 'O']
+    dir_offsets = {
+            'N': [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),  (0, 0),  (0, 1),
+                  (1, -1),  (1, 0),  (1, 1)],
+            'S': [(1, 1),   (1, 0),   (1, -1),
+                  (0, 1),   (0, 0),   (0, -1),
+                  (-1, 1),  (-1, 0),  (-1, -1)],
+            'L': [(-1, 1),  (0, 1),   (1, 1),
+                  (-1, 0),  (0, 0),   (1, 0),
+                  (-1, -1), (0, -1),  (1, -1)],
+            'O': [(1, -1),  (0, -1),  (-1, -1),
+                  (1, 0),   (0, 0),   (-1, 0),
+                  (1, 1),   (0, 1),   (-1, 1)]
+        }
+    offsets = dir_offsets[self.direction]
+
+    for i in range (3):
+        for j in range(3):
+            dy, dx = offsets[i * 3 + j]
+            map_pos = (current_pos[0] + dy, current_pos[1] + dx)
+            cell_value = sensor_data [i][j]
+
+            if i == 1 and j == 1:
+                self.memory[map_pos] = '_'
+            elif cell_value not in directions:
+                self.memory[map_pos] = cell_value
+
+                if cell_value == '0':
+                    self.food_locations.add(map_pos)
+                elif cell_value == 'S':
+                    self.exit_locations = map_pos
+    
+    self.visited.add(current_pos)
+
